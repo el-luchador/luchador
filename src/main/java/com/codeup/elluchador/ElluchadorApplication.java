877f47dfc72a14c1502ed0ee091d183dc09e00e4
J.R <juan.delarosa1224@gmail.com>
2019-02-11 15:34:08 -0600

    first commit

---
+++ src/main/java/com/codeup/elluchador/ElluchadorApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.elluchador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElluchadorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElluchadorApplication.class, args);
+    }
+
+}
+
