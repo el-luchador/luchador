684628caab31efd9c0d7f6a7f7a450094ddaad3b
J.R <juan.delarosa1224@gmail.com>
2019-02-12 13:12:52 -0600

    constructor/getters and setters

--- src/main/java/com/codeup/elluchador/models/Admin.java
+++ src/main/java/com/codeup/elluchador/models/Admin.java
@@ -1,11 +1,42 @@
 package com.codeup.elluchador.models;
 
 
-import org.hibernate.annotations.Table;
 import javax.persistence.*;
 import javax.persistence.Entity;
 
 @Entity
 @Table(name = "Admin")
 public class Admin {
+
+    @Column
+    private String name;
+
+    @Column
+    private String username;
+
+    @Column
+    private Boolean isAdmin;
+
+
+
+    public Admin () {
+    }
+
+    public Admin(String name, String username, Boolean isAdmin) {
+        this.name = name;
+        this.username = username;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public Boolean getAdmin() { return isAdmin; }
+
+    public void setAdmin(Boolean admin) { isAdmin = admin; }
 }
--- src/main/java/com/codeup/elluchador/models/User.java
+++ src/main/java/com/codeup/elluchador/models/User.java
@@ -1,4 +1,39 @@
 package com.codeup.elluchador.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
 public class User {
+
+    @Column
+    private String name;
+
+    @Column
+    private String username;
+
+    @Column
+    private Boolean isAdmin;
+
+    public User(String name, String username, Boolean isAdmin) {
+        this.name = name;
+        this.username = username;
+        this.isAdmin = isAdmin;
+    }
+
+    public User(){}
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public Boolean getAdmin() { return isAdmin; }
+
+    public void setAdmin(Boolean admin) { isAdmin = admin; }
 }
