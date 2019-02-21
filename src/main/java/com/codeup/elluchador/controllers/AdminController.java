dc03800d64aae0317419d39d21dac1d07a9cbf5f
J.R <juan.delarosa1224@gmail.com>
2019-02-21 13:23:15 -0600

    styling is back to the way you had it

--- src/main/java/com/codeup/elluchador/controllers/AdminController.java
+++ src/main/java/com/codeup/elluchador/controllers/AdminController.java
@@ -1,15 +1,16 @@
 package com.codeup.elluchador.controllers;
 
 import com.codeup.elluchador.repositories.AdminRepository;
-import com.codeup.elluchador.repositories.UserRepository;
-//import com.codeup.elluchador.repositories.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class AdminController {
-    private UserRepository userDao;
     private final AdminRepository adminDao;
 
     public AdminController (AdminRepository adminDao) {
-//        this.userDao = userDao;
         this.adminDao = adminDao;
+    }
+    @GetMapping("/")
+    public String homePage() {
+        return "/partials";
     }
 }
--- src/main/java/com/codeup/elluchador/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeup.elluchador.repositories;
-
-import com.codeup.elluchador.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    public User findById (String user);
-}
