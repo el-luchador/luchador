cff33cf2f9582f65f1dc15fe036512eab0af6a31
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-02-28 21:39:40 -0600

    added site-content.html, refactored partials.html to only include fragments, moved all other code in partials to site-content.html, connected partials to landing.html and site-content.html, added code to ElluchadorApplication.java to get images to load properly

--- src/main/java/com/codeup/elluchador/ElluchadorApplication.java
+++ src/main/java/com/codeup/elluchador/ElluchadorApplication.java
@@ -2,6 +2,10 @@ package com.codeup.elluchador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ElluchadorApplication {
@@ -10,5 +14,15 @@ public class ElluchadorApplication {
         SpringApplication.run(ElluchadorApplication.class, args);
     }
 
+    @Bean
+    WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**")
+                        .addResourceLocations("classpath:/static/");
+            }
+        };
+    }
 }
 
--- src/main/java/com/codeup/elluchador/controllers/AdminController.java
+++ src/main/java/com/codeup/elluchador/controllers/AdminController.java
@@ -1,18 +1,22 @@
 package com.codeup.elluchador.controllers;
 
 import com.codeup.elluchador.repositories.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
-    private final AdminRepository adminDao;
 
-    public AdminController (AdminRepository adminDao) {
-        this.adminDao = adminDao;
-    }
+    @Autowired
+    private AdminRepository adminDao;
+
+//    public AdminController (AdminRepository adminDao) {
+//        this.adminDao = adminDao;
+//    }
+
     @GetMapping("/")
     public String homePage() {
-        return "system/landing";
+        return "/landing";
     }
 }
